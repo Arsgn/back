12e5c6f9e053c81e7f8337335e8d58d8e920407d
1234 <a1234@MacBook-Air-1234.local>
2026-01-17 12:38:54 +0600

    new

--- src/modules/about/about.routes.ts
+++ src/modules/about/about.routes.ts
@@ -5,7 +5,7 @@ const aboutRoutes = Router();
 
 aboutRoutes.get("/get", aboutControllers.getAbout);
 aboutRoutes.post("/post", aboutControllers.postAbout);
-aboutRoutes.delete("/delete", aboutControllers.deleteAbout);
+aboutRoutes.delete("/delete/:id", aboutControllers.deleteAbout);
 aboutRoutes.put("/put", aboutControllers.putAbout);
 
 export default aboutRoutes;
