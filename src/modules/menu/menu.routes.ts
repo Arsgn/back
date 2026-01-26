0e4e58f9ac49903a8d35981dd29496bcee8abe4b
Arsgn <blackbarber1323@gmail.com>
2026-01-26 16:42:04 +0600

    id

--- src/modules/menu/menu.routes.ts
+++ src/modules/menu/menu.routes.ts
@@ -4,9 +4,11 @@ import menuControllers from "./menu.controllers";
 const menuRoutes = Router();
 
 menuRoutes.get("/get", menuControllers.getMenu);
+menuRoutes.get("/get/:id", menuControllers.getMenuById);
 menuRoutes.get("/search", menuControllers.searchMenu);
 menuRoutes.post("/post", menuControllers.postMenu);
 menuRoutes.delete("/delete/:id", menuControllers.deleteMenu);
 menuRoutes.put("/put/:id", menuControllers.putMenu);
 
 export default menuRoutes;
+
